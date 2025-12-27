3d601310d890b65a35b8aefed47a12e97efafa14
wilhelmusfao <104365841+wilhelmusfao04@users.noreply.github.com>
2025-12-27 23:33:24 +0700

    Create SplashActivity.kt

---
+++ SplashActivity.kt
@@ -0,0 +1,21 @@
+// SplashActivity.kt
+package com.example.sewamobil
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1200L)
+    }
+}
