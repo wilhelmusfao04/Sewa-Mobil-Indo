6f8e100eec39e8c34255de6e1233135e911948ca
wilhelmusfao <104365841+wilhelmusfao04@users.noreply.github.com>
2025-12-28 00:01:35 +0700

    Create ApiClient.kt

---
+++ ApiClient.kt
@@ -0,0 +1,33 @@
+// network/ApiClient.kt
+package com.example.sewamobil.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+
+    private const val BASE_URL = "https://api.sewamobil.com/"
+
+    private val okHttp: OkHttpClient by lazy {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BASIC
+        }
+        OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttp)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val mobilApi: MobilApi by lazy {
+        retrofit.create(MobilApi::class.java)
+    }
+}
